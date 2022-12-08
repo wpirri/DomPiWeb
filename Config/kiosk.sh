cdfe0681368c31abadd4545afb20b4e6bca055fa
Walter Pirri <walter@pirri.com.ar>
2022-12-08 20:29:52 -0300

    Diferencias segun browser instalado
    Diferencias segun browser instalado

--- Config/kiosk.sh
+++ Config/kiosk.sh
@@ -8,4 +8,10 @@ unclutter -idle 0.5 -root &
 sed -i 's/"exited_cleanly":false/"exited_cleanly":true/' /home/pi/.config/chromium/Default/Preferences
 sed -i 's/"exit_type":"Crashed"/"exit_type":"Normal"/' /home/pi/.config/chromium/Default/Preferences
 
-/usr/bin/chromium-browser --noerrdialogs --disable-infobars --kiosk http://192.168.10.24/
+rm -rf /home/pi/.cache/?hromium/?efault/?ache/*
+
+if [ -x /usr/bin/chromium-browser ]; then
+        /usr/bin/chromium-browser --noerrdialogs --disable-infobars --kiosk http://192.168.10.24/
+else
+        /usr/bin/chromium --noerrdialogs --disable-infobars --kiosk http://192.168.10.24/
+fi
