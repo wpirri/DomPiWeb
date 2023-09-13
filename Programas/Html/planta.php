497cf880d2bb41d46a37d2f4f0af927dd458e82c
Walter Pirri <walter@pirri.com.ar>
2023-09-13 09:31:11 -0300

    Evitar que se corte el loop de adquisición de datos por errores
    Evitar que se corte el loop de adquisición de datos por errores

--- Programas/Html/planta.php
+++ Programas/Html/planta.php
@@ -36,18 +36,20 @@ include("obj_draw.php");
 <script type="text/javascript" >
 
 function updateHomePicture(msg) {
-	jsonData = JSON.parse(msg).response;
-	if(jsonData) {
-		var bg_data = jsonData[0].Planta1.split(';');
-		document.getElementById('plano1').src = 'images/' + bg_data[0];
-		if(bg_data[1] != null && bg_data[2] != null)
-		{
-			setTimeout("ScrollMap(" + bg_data[1] + "," + bg_data[2] + ");", 1000);
+	try {
+		jsonData = JSON.parse(msg).response;
+		if(jsonData) {
+			var bg_data = jsonData[0].Planta1.split(';');
+			document.getElementById('plano1').src = 'images/' + bg_data[0];
+			if(bg_data[1] != null && bg_data[2] != null)
+			{
+				setTimeout("ScrollMap(" + bg_data[1] + "," + bg_data[2] + ");", 1000);
+			}
 		}
-	}
-	else {
-		newAJAXCommand('/cgi-bin/abmsys.cgi?funcion=get_current', updateHomePicture, false);
-	}
+		else {
+			newAJAXCommand('/cgi-bin/abmsys.cgi?funcion=get_current', updateHomePicture, false);
+		}
+	} catch (e) { return; }
 }
 
 function ScrollMap(x, y) {
@@ -57,43 +59,45 @@ function ScrollMap(x, y) {
 }
 
 function updateHomeStatus(msg) {
-	jsonData = JSON.parse(msg).response;
+	try {
+		jsonData = JSON.parse(msg).response;
 
-	if(!jsonData) return;
+		if(!jsonData) return;
 
-	for (var i = 0; i < jsonData.length; i++) { 
-		//jsonData[i].Objeto
-		//jsonData[i].Port
-		//jsonData[i].Icono_Apagado
-		//jsonData[i].Icono_Encendido
-		//jsonData[i].Estado
-		//jsonData[i].Tipo
-		//jsonData[i].Perif_Data
-		var Objeto = jsonData[i].Objeto;
-		// Sustituciones (Mantener: obj_style.php objdraw.php planta.php)
-		var objId = Objeto.replace(/ /g, '').replace(/\./g, '');
-		if ( jsonData[i].Tipo == 2 ) {
-			document.getElementById('id-'+objId).innerHTML = '&nbsp' + jsonData[i].Estado + '&nbsp';
-		} else if ( jsonData[i].Tipo == 6 ) {
-			if(jsonData[i].Port[0] == 'T') {
-				document.getElementById('id-'+objId).innerHTML = '&nbsp;T ' + jsonData[i].Perif_Data + ' °C&nbsp;';
-			} else if(jsonData[i].Port[0] == 'H') {
-				document.getElementById('id-'+objId).innerHTML = '&nbsp;Hr ' + jsonData[i].Perif_Data + ' %&nbsp;';
+		for (var i = 0; i < jsonData.length; i++) { 
+			//jsonData[i].Objeto
+			//jsonData[i].Port
+			//jsonData[i].Icono_Apagado
+			//jsonData[i].Icono_Encendido
+			//jsonData[i].Estado
+			//jsonData[i].Tipo
+			//jsonData[i].Perif_Data
+			var Objeto = jsonData[i].Objeto;
+			// Sustituciones (Mantener: obj_style.php objdraw.php planta.php)
+			var objId = Objeto.replace(/ /g, '').replace(/\./g, '');
+			if ( jsonData[i].Tipo == 2 ) {
+				document.getElementById('id-'+objId).innerHTML = '&nbsp' + jsonData[i].Estado + '&nbsp';
+			} else if ( jsonData[i].Tipo == 6 ) {
+				if(jsonData[i].Port[0] == 'T') {
+					document.getElementById('id-'+objId).innerHTML = '&nbsp;T ' + jsonData[i].Perif_Data + ' °C&nbsp;';
+				} else if(jsonData[i].Port[0] == 'H') {
+					document.getElementById('id-'+objId).innerHTML = '&nbsp;Hr ' + jsonData[i].Perif_Data + ' %&nbsp;';
+				} else {
+					document.getElementById('id-'+objId).innerHTML = '&nbsp;' + jsonData[i].Perif_Data + '&nbsp;';
+				}
 			} else {
-				document.getElementById('id-'+objId).innerHTML = '&nbsp;' + jsonData[i].Perif_Data + '&nbsp;';
-			}
-		} else {
-			if(jsonData[i].Estado == 0) {
-				document.getElementById('id-'+objId).src = 'images/' + jsonData[i].Icono_Apagado;
-			} else if(jsonData[i].Estado == 1) {
-				document.getElementById('id-'+objId).src = 'images/' + jsonData[i].Icono_Encendido;
-			} else if(jsonData[i].Estado == 2) {
-				document.getElementById('id-'+objId).src = 'images/' + jsonData[i].Icono_Auto;
-			} else {
-				document.getElementById('id-'+objId).src = 'images/no.png';
+				if(jsonData[i].Estado == 0) {
+					document.getElementById('id-'+objId).src = 'images/' + jsonData[i].Icono_Apagado;
+				} else if(jsonData[i].Estado == 1) {
+					document.getElementById('id-'+objId).src = 'images/' + jsonData[i].Icono_Encendido;
+				} else if(jsonData[i].Estado == 2) {
+					document.getElementById('id-'+objId).src = 'images/' + jsonData[i].Icono_Auto;
+				} else {
+					document.getElementById('id-'+objId).src = 'images/no.png';
+				}
 			}
 		}
-	}
+	} catch (e) { return; }
 }
 
 function OnLoad( ) {
