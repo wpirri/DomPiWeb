6ec4164ce9cada6bef30d07171ae6924df6ad0de
Walter Pirri <walter@pirri.com.ar>
2025-10-26 11:20:27 -0300

    Correcciones en init de base
    Correcciones en init de base

--- DataBase/init.sql
+++ DataBase/init.sql
@@ -1,5 +1,9 @@
 USE DB_DOMPIWEB;
 
+CREATE USER 'dompi_web'@'%' IDENTIFIED BY 'dompi_web'; 
+GRANT SELECT, INSERT, UPDATE, DELETE ON DB_DOMPIWEB.* TO 'dompi_web'@'%' WITH GRANT OPTION;
+FLUSH PRIVILEGES;
+
 INSERT INTO TB_DOM_PERIF (Id, MAC, Dispositivo) VALUES (0, '0000000000000000', 'Ninguno');
 INSERT INTO TB_DOM_GRUPO_VISUAL(Id, Nombre) VALUES (0, 'Ninguno');
 INSERT INTO TB_DOM_GRUPO_VISUAL(Id, Nombre) VALUES (1, 'Alarma');
@@ -11,10 +15,10 @@ INSERT INTO TB_DOM_GRUPO_VISUAL(Id, Nombre) VALUES (6, 'Riego');
 INSERT INTO TB_DOM_ASSIGN (Id, Objeto, Dispositivo, Port, Tipo) VALUES(0,'Ninguno','0',0,0);
 INSERT INTO TB_DOM_GROUP (Id, Grupo) VALUES (0, 'Ninguno');
 INSERT INTO TB_DOM_FLAG (Id, Variable) VALUES (0, 'Ninguna');
-INSERT INTO TB_DOM_EVENT (Id, Evento) VALUES (0,'Ninguno');
 INSERT INTO TB_DOM_ALARM_PARTICION (Id, Nombre, Entrada_Act_Total, Entrada_Act_Parcial, Testigo_Activacion) VALUES (0, 'Ninguna', 0, 0, 0);
 INSERT INTO TB_DOM_ALARM_ZONA (Id, Particion, Objeto_Zona) VALUES (0, 0, 0);
 INSERT INTO TB_DOM_ALARM_SALIDA (Id, Particion, Objeto_Salida) VALUES (0, 0, 0);
+INSERT INTO TB_DOM_EVENT (Id, Evento) VALUES (0,'Ninguno');
 INSERT INTO TB_DOM_USER (id, Usuario, Nombre_Completo, Pin_WEB) VALUES (0, 'nadie', 'Nadie', '****');
 INSERT INTO TB_DOM_USER (id, Usuario, Nombre_Completo, Pin_WEB) VALUES (1, 'admin', 'Administrador del sistema', 'admin');
 INSERT INTO TB_DOM_CONFIG (id, Creacion, System_Key, Cloud_Host_1_Address, Cloud_Host_1_Proto, Cloud_Host_2_Address, Cloud_Host_2_Proto, Planta1)
@@ -23,6 +27,3 @@ INSERT INTO TB_DOM_AUTO (Id, Objeto, Tipo) VALUES(0,'Ninguno',1);
 INSERT INTO TB_DOM_AT (Id, Agenda, Objeto_Destino, Grupo_Destino, Variable_Destino) VALUES (0,'Ninguna', 0, 0, 0);
 INSERT INTO TB_DOM_CAMARA (Id, Nombre) VALUES (0, 'Ninguna');
 
-CREATE USER 'dompi_web'@'%' IDENTIFIED BY 'dompi_web'; 
-GRANT SELECT, INSERT, UPDATE, DELETE ON DB_DOMPIWEB.* TO 'dompi_web'@'%' WITH GRANT OPTION;
-FLUSH PRIVILEGES;
